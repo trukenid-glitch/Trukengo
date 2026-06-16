f5561a047727d785f7790d428f69499da127ebd9
trukenid-glitch <trukenid@gmail.com>
2026-06-16 09:49:16 +0700

    add analistic and speed insight

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "./index.css";
 import App from "./App.jsx";
 
@@ -11,6 +13,8 @@ createRoot(document.getElementById("root")).render(
       {" "}
       {/* <-- Bungkus App kamu di sini */}
       <App />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>,
 );
